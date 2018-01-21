81adb554861282966ac00cc6bd9d68b13a82b64b
Abner Carleto <abner.carleto@ymail.com>
2018-01-21 01:06:37 -0200

    add react-grid-system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-grid-system';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Test</h1>
+        <Container>
+          <Row>
+            <Col sm={5}>
+              test
+            </Col>
+            <Col sm={5}>
+              Test 2
+            </Col>
+            <Col>
+              Test 3
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
